177fa55f3c68892f1119ee35a6e844a4d916edd6
Maxamed459 <maxamedmahdi459@gmail.com>
2026-05-22 20:52:22 +0300

    refactor userController to use service methods and improve response handling

--- controller/userController.js
+++ controller/userController.js
@@ -1,34 +1,48 @@
-import User from "../model/user.js";
+import {
+  authenticateUser,
+  createUser,
+  getAllUsers,
+} from "../services/userService.js";
+import { successResponse } from "../utils/apiResponse.js";
+import { signToken } from "../utils/jwt.js";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
   try {
-    // success
+    const user = await createUser(req.body);
 
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const isUserExists = await User.findOne({
-      $or: [
-        { email: email.toLowerCase() },
-        { username: username.toLowerCase() },
-      ],
-    });
-
-    if (isUserExists) {
-      return res.send("This user or email already exists");
-    }
-    const userinfo = new User({
-      email: email,
-      username: username,
-      password: password,
-    });
-
-    await userinfo.save();
-    userinfo.password = undefined;
-    return res.status(201).send(userinfo);
+    return successResponse(res, 201, "User registered successfully", { user });
   } catch (error) {
-    res.send("something went wrong: ", error);
-    console.log(`Error: ${error}`);
+    return next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const user = await authenticateUser(req.body);
+    const token = signToken(user);
+
+    return successResponse(res, 200, "Login successful", { token, user });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getProfile = async (req, res, next) => {
+  try {
+    return successResponse(res, 200, "Profile fetched successfully", {
+      user: req.user,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await getAllUsers();
+
+    return successResponse(res, 200, "Users fetched successfully", { users });
+  } catch (error) {
+    return next(error);
   }
 };
