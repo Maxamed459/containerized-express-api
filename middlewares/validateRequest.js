431219d0caed92105ce860628fc88c51922cb0d2
Maxamed459 <maxamedmahdi459@gmail.com>
2026-05-22 20:52:50 +0300

    add request validation middleware to handle input validation errors

---
+++ middlewares/validateRequest.js
@@ -0,0 +1,19 @@
+import { validationResult } from "express-validator";
+import AppError from "../utils/AppError.js";
+
+const validateRequest = (req, res, next) => {
+  const result = validationResult(req);
+
+  if (result.isEmpty()) {
+    return next();
+  }
+
+  const errors = result.array().map((error) => ({
+    field: error.path,
+    message: error.msg,
+  }));
+
+  return next(new AppError("Validation failed", 400, errors));
+};
+
+export default validateRequest;
