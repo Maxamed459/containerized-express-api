2c0c62f3e348390552e20c728df4d358d65a022e
Maxamed459 <maxamedmahdi459@gmail.com>
2026-05-22 20:53:58 +0300

    add authentication validators for user registration and login

---
+++ validators/authValidators.js
@@ -0,0 +1,29 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long"),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .normalizeEmail(),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body("role")
+    .optional()
+    .isIn(["admin", "user"])
+    .withMessage("Role must be either admin or user"),
+];
+
+export const loginValidator = [
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .normalizeEmail(),
+  body("password").notEmpty().withMessage("Password is required"),
+];
