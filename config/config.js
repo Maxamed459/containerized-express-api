1daae154db1988fc91f8a84f7820ede130bda933
Maxamed459 <maxamedmahdi459@gmail.com>
2026-05-22 20:51:34 +0300

    add jwtConfig export to config.js for JWT settings

--- config/config.js
+++ config/config.js
@@ -3,4 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbUrl = process.env.MangoUrl;
+
+export const jwtConfig = {
+  secret: process.env.JWT_SECRET || "change-this-jwt-secret",
+  expiresIn: process.env.JWT_EXPIRES_IN || "1d",
+};
+
 export default dbUrl;
